1c67b7959f9e5696fe487df58042d809dff0e438
SonyFriend <53859597+SonyFriend@users.noreply.github.com>
2019-08-12 13:59:07 +0800

    Update Land.py

--- Land.py
+++ Land.py
@@ -11,10 +11,11 @@ import cv2
 import csv
 import multiprocessing
 import matplotlib.pyplot as plt
-###########################################################################
+###################!!!!!!!data preprocessing!!!!!!!!##############################
+#original data size is about 1 million. I only use 5000 because my computer can't bear
 train=pd.read_csv('C:/Users/user/Downloads/pythonCode/Landmark/train.csv')
 train=train[~train['url'].isin(['None'])]
-new_train=train.iloc[0:9999,:]
+new_train=train.iloc[0:5000,:]
 new_train.index=range(0,len(new_train))
 
 x=0
@@ -22,11 +23,11 @@ for i in range(len(new_train)):
 	try:
 		link=new_train['url'][i]
 		local=os.path.join('D:/LandGraph/%s.jpg' % x)
-		urllib.request.urlretrieve(link,local)
+		urllib.request.urlretrieve(link,local) #Download image from url
 		x+=1
 	except OSError:
 		pass
-		new_train=new_train.drop(index=i)
+		new_train=new_train.drop(index=i) #Because some image have a problem( 404 not found)
 	continue
 
 with open('C:/Users/user/Downloads/pythonCode/Landmark/new_train.csv','w',newline='') as f:
