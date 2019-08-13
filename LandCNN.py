9fa2199212ed835c0f42cb78dcdb846b314a6c83
SonyFriend <53859597+SonyFriend@users.noreply.github.com>
2019-08-13 17:14:55 +0800

    Update LandCNN.py

--- LandCNN.py
+++ LandCNN.py
@@ -12,13 +12,15 @@ import cv2
 import csv
 import matplotlib.pyplot as plt
 ###############################################################################################
-#https://bulkresizephotos.com/zh-tw <--- This website can resize your image to 32*32
-new_train=pd.read_csv('C:/Users/user/Downloads/pythonCode/Landmark/new_train_id1_1000.csv')
+#https://bulkresizephotos.com/zh-tw <- This website can change your image to 32*32 pixels
+new_train=pd.read_csv('C:/Users/user/Downloads/pythonCode/Landmark/new_train_id0_499.csv')
 img=[]
-filename=os.listdir("D:/LandGraphNew_1_1000")
+filename=os.listdir("D:/LandGraphNew_0_499")
 for file in filename:
-		img.append(np.array(Image.open("D:/LandGraphNew_1_1000/"+file)))
+		img.append(np.array(Image.open("D:/LandGraphNew_0_499/"+file)))
 img=np.array(img)
+
+
 ###########################################################################################
 np.random.seed(1337)
 from keras.utils import np_utils
@@ -26,11 +28,11 @@ from keras.models import Sequential
 from keras.layers import Convolution2D,Dense,MaxPool2D,Activation,Dropout,Flatten
 from keras.optimizers import Adam
 from sklearn.model_selection import train_test_split
-X_train,X_test,y_train,y_test=train_test_split(img1,new_train['landmark_id'],test_size=0.2)
+X_train,X_test,y_train,y_test=train_test_split(img,new_train['landmark_id'],test_size=0.2)
 X_train=X_train.reshape(-1,32,32,3)/255 #Normalize
 X_test=X_test.reshape(-1,32,32,3)/255
-y_train=np_utils.to_categorical(y_train,num_classes=max(new_train['landmark_id'])+1)
-y_test=np_utils.to_categorical(y_test,num_classes=max(new_train['landmark_id'])+1)
+y_train=np_utils.to_categorical(y_train,num_classes=500)
+y_test=np_utils.to_categorical(y_test,num_classes=500)
 ########################################################################################
 model=Sequential()
 model.add(Convolution2D(filters=32,kernel_size=(3,3),input_shape=(32,32,3),activation='relu',padding='same'))
@@ -46,11 +48,11 @@ model.add(Flatten())
 model.add(Dense(1024,activation='relu'))
 model.add(Dropout(rate=0.25))
 
-model.add(Dense(max(new_train['landmark_id'])+1,activation='softmax'))
+model.add(Dense(500,activation='softmax'))
 
 model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
 
-train_history=model.fit(X_train,y_train,validation_split=0.3,epochs=20,batch_size=100,verbose=1)
+train_history=model.fit(X_train,y_train,validation_split=0.2,epochs=40,batch_size=100,verbose=1)
 accuracy=model.evaluate(X_test,y_test,verbose=1)
 print(accuracy[1])
 
